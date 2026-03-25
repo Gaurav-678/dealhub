4198ee43a87a8f5f701535da8f76c3fca69748cb
Gaurav-678 <neelam890123@gmail.com>
2026-03-25 14:39:48 +0530

    Update product.js

--- product.js
+++ product.js
@@ -40,3 +40,13 @@ const products = [
         featured: false
     }
 ];
+{
+    id: 5,
+    name: "Orange Enzyme Face Cleanser",
+    category: "Beauty",
+    image: "https://m.media-amazon.com/images/I/61i-gYQXJvL._SL1500_.jpg",
+    amazonPrice: "₹299",
+    flipkartPrice: "₹249 🔥",
+    link: "https://amzn.to/4uVbl7m",
+    featured: true
+}
