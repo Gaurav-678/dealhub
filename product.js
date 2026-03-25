cbdfee11b088ae86cd2de82946cf856de838cb87
Gaurav-678 <neelam890123@gmail.com>
2026-03-25 14:48:15 +0530

    Update product.js

--- product.js
+++ product.js
@@ -6,7 +6,7 @@ const products = [
     category: "Beauty",
     image: "https://m.media-amazon.com/images/I/61i-gYQXJvL._SL1500_.jpg",
     amazonPrice: "₹299",
-    flipkartPrice: "₹249 🔥",
+    flipkartPrice: "₹249 ",
     link: "https://amzn.to/4uVbl7m",
     featured: true
 }
