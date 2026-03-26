c62802b81558be9c50c9a58e0aeae741679b6efe
Gaurav-678 <neelam890123@gmail.com>
2026-03-26 12:26:16 +0530

    Update products.js

--- products.js
+++ products.js
@@ -8,5 +8,15 @@ const products = [
     flipkartPrice: "₹249",
     link: "https://amzn.to/4uVb17m",
     featured: true
+  },
+  {
+    id: 2,
+    name: "Ghar soap",
+    category: "Beauty",
+    image: "https://m.media-amazon.com/images/I/718F+0RgtfL._SL1500_.jpg",
+    amazonPrice: "371",
+    flipkartPrice: "371",
+    link: "https://amzn.to/4t7llbJ",
+    featured: true
   }
 ];
